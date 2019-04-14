01b158dd3d377a5c219b929026ac21c66788a66d
viniciusloner <viniciusloner@gmail.com>
2019-04-14 14:04:48 -0300

    alterações

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const cors = require('cors')
 const app = express()
 app.use(cors)
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
+//const io = require('socket.io')(server)
 
-io.on('connection', socket => {
+/* io.on('connection', socket => {
     socket.on('connectRoom', box =>{
         socket.join(box)
     })
 })
-
+ */
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     req.io = io
     return next()
-})
+}) */
 
 app.get('/', (req, res) => {
     res.send('Hello World')

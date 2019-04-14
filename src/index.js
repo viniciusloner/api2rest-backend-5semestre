30926ad0568ae57937f4e78e656ad59f7e01bc52
viniciusloner <viniciusloner@gmail.com>
2019-04-14 14:44:05 -0300

    alterações

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-app.use(cors)
+app.use(cors())
 const server = require('http').Server(app)
 //const io = require('socket.io')(server)
 
